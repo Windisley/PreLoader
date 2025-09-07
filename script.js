bacbeb10be35bffcc26c47ae59c7f7b739e0cff4
Windisley <windisleyl@gmail.com>
2025-09-07 19:02:48 -0300

    Codigo Pronto

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const containerLoad = document.querySelector(".cont-load")
+const container = document.querySelector(".container")
+
 window.addEventListener("load", ()=>{
     containerLoad.remove()
+    container.style.display = "flex" 
 })
